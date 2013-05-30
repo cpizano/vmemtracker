21da46a0662cd57b87cca40220d3fb62b3a2c39f
Carlos Pizano <carlos.pizano@gmail.com>
2013-05-29 19:23:37 -0700

    Some cleanup

--- src/main.cc
+++ src/main.cc
@@ -75,6 +75,86 @@ HWND MakeWindow(const wchar_t* title, ULONG style, HWND parent,
                            parent, menu, ThisModule(), NULL); 
 }
 
+
+bool WaitForFileAccess(const wchar_t* bin_to_track) {
+  OVERLAPPED ov = {0};
+  ov.hEvent = ::CreateEventW(nullptr, FALSE, FALSE, nullptr);
+
+  HANDLE file = CreateFileW(bin_to_track, GENERIC_READ, FILE_SHARE_DELETE,
+                            nullptr, OPEN_EXISTING,
+                            FILE_FLAG_OVERLAPPED, nullptr);
+  if (file == INVALID_HANDLE_VALUE) {
+    return false;
+  }
+
+  REQUEST_OPLOCK_INPUT_BUFFER ibuff = {
+    REQUEST_OPLOCK_CURRENT_VERSION,
+    sizeof(ibuff),
+    OPLOCK_LEVEL_CACHE_READ | OPLOCK_LEVEL_CACHE_HANDLE,
+    REQUEST_OPLOCK_INPUT_FLAG_REQUEST,
+  };
+
+  REQUEST_OPLOCK_OUTPUT_BUFFER obuff = {
+    REQUEST_OPLOCK_CURRENT_VERSION,
+    sizeof(obuff),
+  };
+
+  ::DeviceIoControl(file, FSCTL_REQUEST_OPLOCK,
+                    &ibuff, sizeof(ibuff),
+                    &obuff, sizeof(obuff),
+                    nullptr, &ov);
+  if (GetLastError() != ERROR_IO_PENDING) {
+    return false;
+  }
+
+  DWORD bytes = 0;
+  if (!GetOverlappedResult(file, &ov, &bytes, TRUE)) {
+    return false;
+  }
+
+  ::CloseHandle(file);
+  ::CloseHandle(ov.hEvent);
+  return true;
+}
+
+struct TrackedProcess {
+  const wchar_t* name;
+  UINT pid;
+  HANDLE ph;
+
+  TrackedProcess(const wchar_t* name, UINT pid, HANDLE ph)
+      : name(name), pid(pid), ph(ph) {
+  }
+};
+
+bool GetProcessesUsingResources(DWORD session_id) {
+  DWORD reason = 0;
+  UINT n_needed = 0;
+  RM_PROCESS_INFO rmpi[64];
+  UINT n_rmpi = _countof(rmpi);
+  Verify(::RmGetList(session_id, &n_needed,
+                     &n_rmpi, rmpi, &reason), ERROR_SUCCESS);
+
+ if (n_rmpi) {
+   // Somebody has the file open.
+   for (size_t ix = 0; ix != n_rmpi; ++n_rmpi) {
+     HANDLE process = ::OpenProcess(PROCESS_QUERY_INFORMATION |
+                                    PROCESS_VM_READ,
+                                    FALSE, rmpi[ix].Process.dwProcessId);
+     if (process) {
+        FILETIME ftCreate, ftExit, ftKernel, ftUser;
+        if (::GetProcessTimes(process, &ftCreate, &ftExit, &ftKernel, &ftUser)) {
+          if (::CompareFileTime(&rmpi[ix].Process.ProcessStartTime, &ftCreate) == 0) {
+            // Process is still alive.
+          }
+        }
+        ::CloseHandle(process);
+     }
+   }
+ } 
+ return false;
+}
+
 int __stdcall wWinMain(HINSTANCE module, HINSTANCE, wchar_t* cc, int) {
 
   if (__argc < 2) {
@@ -83,80 +163,18 @@ int __stdcall wWinMain(HINSTANCE module, HINSTANCE, wchar_t* cc, int) {
 
   const wchar_t* bin_to_track = __wargv[1];
 
+  DWORD session_id = 0;
+  wchar_t session_key[CCH_RM_SESSION_KEY + 1] = { 0 };
+  Verify(::RmStartSession(&session_id, 0, session_key), ERROR_SUCCESS);
 
- DWORD session_id = 0;
- wchar_t session_key[CCH_RM_SESSION_KEY + 1] = { 0 };
- Verify(::RmStartSession(&session_id, 0, session_key), ERROR_SUCCESS);
+  Verify(::RmRegisterResources(session_id, 1, &bin_to_track,
+                               0, NULL, 0, NULL), ERROR_SUCCESS);
 
- Verify(::RmRegisterResources(session_id, 1, &bin_to_track,
-                              0, NULL, 0, NULL), ERROR_SUCCESS);
-
- DWORD reason = 0;
- UINT i;
- UINT n_needed = 0;
- RM_PROCESS_INFO rmpi[64];
- UINT n_rmpi = _countof(rmpi);
- Verify(::RmGetList(session_id, &n_needed,
-                    &n_rmpi, rmpi, &reason), ERROR_SUCCESS);
-
- if (n_rmpi) {
-   // Somebody has the file open.
-   for (size_t ix = 0; ix != n_rmpi; ++n_rmpi) {
-     HANDLE process = ::OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION,
-                                    FALSE, rmpi[i].Process.dwProcessId);
-     if (process) {
-        FILETIME ftCreate, ftExit, ftKernel, ftUser;
-        if (::GetProcessTimes(process, &ftCreate, &ftExit, &ftKernel, &ftUser)) {
-          if (::CompareFileTime(&rmpi[i].Process.ProcessStartTime, &ftCreate) == 0) {
-            // Got a live process.
-          }
-        }
-        ::CloseHandle(process);
-     }
-   }
-
- } else {
-   // Wait for the file to be open.
-
-    OVERLAPPED ov = {0};
-    ov.hEvent = ::CreateEventW(nullptr, FALSE, FALSE, nullptr);
-
-    HANDLE file = CreateFileW(bin_to_track, GENERIC_READ, FILE_SHARE_DELETE,
-                              nullptr, OPEN_EXISTING,
-                              FILE_FLAG_OVERLAPPED, nullptr);
-    if (file == INVALID_HANDLE_VALUE) {
-      return 2;
-    }
-
-    REQUEST_OPLOCK_INPUT_BUFFER ibuff = {
-      REQUEST_OPLOCK_CURRENT_VERSION,
-      sizeof(ibuff),
-      OPLOCK_LEVEL_CACHE_READ | OPLOCK_LEVEL_CACHE_HANDLE,
-      REQUEST_OPLOCK_INPUT_FLAG_REQUEST,
-    };
-
-    REQUEST_OPLOCK_OUTPUT_BUFFER obuff = {
-      REQUEST_OPLOCK_CURRENT_VERSION,
-      sizeof(obuff),
-    };
-
-    ::DeviceIoControl(file, FSCTL_REQUEST_OPLOCK,
-                      &ibuff, sizeof(ibuff),
-                      &obuff, sizeof(obuff),
-                      nullptr, &ov);
-    if (GetLastError() != ERROR_IO_PENDING) {
-      return 3;
-    }
-
-    DWORD bytes = 0;
-    if (!GetOverlappedResult(file, &ov, &bytes, TRUE)) {
-      return 0;
-    }
-
-    ::CloseHandle(file);
-    ::CloseHandle(ov.hEvent);
- }
-
+  if (GetProcessesUsingResources(session_id)) {
+  } else {
+   WaitForFileAccess(bin_to_track);
+  }
+ 
 #if 0
   MessageHandler msg_handlers[] = {
     { WM_CLOSE, [] (HWND window, WPARAM, LPARAM) -> LRESULT {
