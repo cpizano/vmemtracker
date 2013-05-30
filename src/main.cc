2d947878f79678c35c6b50f2715d7fecfc7df1e4
Carlos Pizano <carlos.pizano@gmail.com>
2013-05-30 15:10:48 -0700

    better version.

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,14 @@
 #include <Windows.h>
+#include <Psapi.h>
 #include <winioctl.h>
 #include <RestartManager.h>
+
 #include <stdlib.h>
+#include <algorithm>
 #include <string>
-#include <map>
+#include <vector>
+#include <memory>
+#include <sstream>
 
 #pragma comment(lib,"rstrtmgr.lib")
 
@@ -89,15 +94,19 @@ struct TrackedProcess {
   HANDLE logfile;
 
   TrackedProcess(const wchar_t* name, RM_UNIQUE_PROCESS id, HANDLE ph)
-      : name(name), id(id), process(ph), logfile(NULL) {
+      : name(name), id(id), process(ph), logfile(INVALID_HANDLE_VALUE) {
   }
-  TrackedProcess()
-      : name(name), id(), process(NULL), logfile(NULL) {
+
+  ~TrackedProcess() {
+    ::CloseHandle(process);
+    ::CloseHandle(logfile);
   }
 };
 
+typedef std::unique_ptr<TrackedProcess> TrackedPtr;
+
 bool GetProcesses(DWORD session_id,
-                  std::map<UINT, TrackedProcess>& tracked_map) {
+                  std::vector<TrackedPtr>& tracked) {
   DWORD reason = 0;
   UINT n_needed = 0;
   RM_PROCESS_INFO rmpi[64];
@@ -105,49 +114,119 @@ bool GetProcesses(DWORD session_id,
   Verify(::RmGetList(session_id, &n_needed,
                      &n_rmpi, rmpi, &reason), ERROR_SUCCESS);
 
-  size_t watched_count = 0;
+  size_t process_count = 0;
 
   for (size_t ix = 0; ix != n_rmpi; ++n_rmpi) {
     const RM_UNIQUE_PROCESS up = rmpi[ix].Process;
     if (up.dwProcessId == ::GetCurrentProcessId())
       continue;
-    // Someone has the file open.
-    HANDLE process = ::OpenProcess(PROCESS_QUERY_INFORMATION |
-                                   PROCESS_VM_READ,
-                                   FALSE, up.dwProcessId);
-    if (process) {
-      FILETIME ftCreate, ftExit, ftKernel, ftUser;
-      if (::GetProcessTimes(process, &ftCreate, &ftExit, &ftKernel, &ftUser)) {
-        if (::CompareFileTime(&up.ProcessStartTime, &ftCreate) == 0) {
-          // Process is still alive.
-          TrackedProcess& tp = tracked_map[up.dwProcessId];
-          if (!tp.process) {
-            tp = TrackedProcess();
-            continue;
-          }
+    if (rmpi[ix].strAppName[0] = 0)
+      continue;
 
-          if (::CompareFileTime(&up.ProcessStartTime, &tp.id.ProcessStartTime) == 0) {
+    // An aplication (not a service) has the file open.
+    HANDLE handle = ::OpenProcess(PROCESS_QUERY_INFORMATION |
+                                  PROCESS_VM_READ,
+                                  FALSE, up.dwProcessId);
+    if (!handle) {
+      //$$ log the event.
+      continue;
+    }
 
-          }
-  
-          ++watched_count;
+    FILETIME ftCreate, ftExit, ftKernel, ftUser;
+    if (::GetProcessTimes(handle, &ftCreate, &ftExit, &ftKernel, &ftUser)) {
+      if (::CompareFileTime(&up.ProcessStartTime, &ftCreate) == 0) {
+        // Process is still alive.
+        ++process_count;
+
+        auto it = std::find_if(begin(tracked), end(tracked),
+            [&up](TrackedPtr& e) {
+                return (e->id.dwProcessId == up.dwProcessId) &&
+                       (::CompareFileTime(&up.ProcessStartTime,
+                                          &e->id.ProcessStartTime) == 0);
+            }
+        );
+
+        if (it == tracked.end()) {
+          // Never seen this one before. Track it.
+          tracked.push_back(TrackedPtr(new TrackedProcess(rmpi[ix].strAppName,
+                                                          rmpi[ix].Process,
+                                                          handle)));
+          continue;
         }
       }
-      ::CloseHandle(process);
     }
+    ::CloseHandle(handle);
   }
-  return (watched_count != 0);
+  return (process_count != 0);
+}
+
+bool LogMemUsage(const wchar_t* dir, TrackedProcess* tracked) {
+  PROCESS_MEMORY_COUNTERS_EX pmcx = {sizeof(pmcx)};
+  if (::GetProcessMemoryInfo(tracked->process, 
+                             reinterpret_cast<PROCESS_MEMORY_COUNTERS*>(&pmcx),
+                             sizeof(pmcx)) == FALSE) {
+    __debugbreak();
+  }
+
+  ULONGLONG time = ::GetTickCount64();
+  DWORD to_write = 0;
+  DWORD written = 0;
+  std::stringstream oss;
+
+  if (tracked->logfile == INVALID_HANDLE_VALUE) {
+    SYSTEMTIME st;
+    ::FileTimeToSystemTime(&tracked->id.ProcessStartTime, &st);
+    wchar_t buff[512];
+    wsprintf(buff, L"%s_%d_%.2d_%.2d_%.2d%.2d", tracked->name.c_str(),
+        st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute);
+    std::wstring path(dir);
+    path.append(L"\\");
+    path.append(buff);
+    DWORD share = FILE_SHARE_READ | FILE_SHARE_DELETE;
+    tracked->logfile = ::CreateFileW(path.c_str(), GENERIC_ALL, share,
+                                     NULL, CREATE_ALWAYS, 0, NULL);
+    if (tracked->logfile == INVALID_HANDLE_VALUE) {
+      // $$ log this.
+      return false;
+    }
+    // Write header.
+
+    oss << "memtrack v1.0.0," << time << "\n";
+    oss << tracked->name.c_str() << "," << tracked->id.dwProcessId;
+    oss << tracked->id.ProcessStartTime.dwHighDateTime << ".";
+    oss << tracked->id.ProcessStartTime.dwLowDateTime << "\n";
+    oss.flush();
+    to_write = oss.str().size();
+    ::WriteFile(tracked->logfile, oss.str().c_str(), to_write, &written, NULL);
+  }
+
+  oss << ::GetTickCount64() << ",";
+  oss << pmcx.PeakWorkingSetSize << L"," << pmcx.WorkingSetSize << L",";
+  oss << pmcx.PrivateUsage << L"\n";
+
+  oss.flush();
+  to_write = oss.str().size();
+  if (!::WriteFile(tracked->logfile, oss.str().c_str(), to_write, &written, NULL)) {
+    return false;
+  }
+
+  DWORD ecode = 0;
+  ::GetExitCodeProcess(tracked->process, &ecode);
+  ::Sleep(1);
+  return true;
 }
 
 int __stdcall wWinMain(HINSTANCE module, HINSTANCE, wchar_t* cc, int) {
 
-  if (__argc < 2) {
+  if (__argc < 3) {
     return 1;
   }
 
   const wchar_t* bin_to_track = __wargv[1];
+  const wchar_t* dir_for_logs = __wargv[2];
 
   // Restart manager initialization.
+  ULONGLONG start = ::GetTickCount64();
 
   DWORD session_id = 0;
   wchar_t session_key[CCH_RM_SESSION_KEY + 1] = { 0 };
@@ -156,18 +235,27 @@ int __stdcall wWinMain(HINSTANCE module, HINSTANCE, wchar_t* cc, int) {
   Verify(::RmRegisterResources(session_id, 1, &bin_to_track,
                                0, NULL, 0, NULL), ERROR_SUCCESS);
 
-
-  ::SetTimer(NULL, 666, 500, NULL); 
-  std::map<UINT, TrackedProcess> tracked_map;
-
+  std::vector<TrackedPtr> tracked;
+  UINT timer = ::SetTimer(NULL, 666, 200, NULL); 
 
   MSG msg = {0};
   while (::GetMessageW(&msg, NULL, 0, 0)) {
 
     if (msg.message == WM_TIMER) {
-      if (tracked_map.empty()) {
-        if(!GetProcesses(session_id, tracked_map))
-          ::SetTimer(NULL, 666, 3000, NULL);
+      GetProcesses(session_id, tracked);
+      if (tracked.empty()) {
+        Verify(::KillTimer(NULL, timer), TRUE);
+        timer = ::SetTimer(NULL, 666, 3000, NULL);
+      } else {
+
+        for (auto it = begin(tracked); it != end(tracked); ++it) {
+          if (!LogMemUsage(dir_for_logs, it->get())) {
+            it = tracked.erase(it);
+          }
+        }
+        
+        Verify(::KillTimer(NULL, timer), TRUE);
+        timer = ::SetTimer(NULL, 666, 500, NULL);
       }
     }
 
